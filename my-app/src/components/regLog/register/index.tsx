73c1593a4567e8b2195a55d88e7872166dd62db0
kiv327 <kacalkinvana87@gmail.com>
2025-10-22 19:54:34 +0300

    react dom + начал делать страницу логина

---
+++ my-app/src/components/regLog/register/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const RegisterPage = () => {
+    return (
+        <div>
+            <h1>Reg page</h1>
+        </div>
+    );
+
+
+
+};
+
+export default RegisterPage;
